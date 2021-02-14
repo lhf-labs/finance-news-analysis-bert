ede095fb7253032443a18ec60161291e350ce121
Asier <asierguti96@gmail.com>
2021-02-14 15:00:20 +0100

    Enhanced deduplication & NA removal

--- src/utils/dedup_csv.py
+++ src/utils/dedup_csv.py
@@ -1,19 +1,11 @@
-# >>> import pandas as pd
-# >>> toclean = pd.read_csv('1606251790_0B3CC61FD4764094EAEA375353022009_G3486172.csv')
-# >>> len(toclean)
-# 1891157
-# >>> deduped = toclean.drop_duplicates('RP_STORY_ID')
-# >>> len(deduped)
-# 701071
-# >>> deduped = deduped.drop_duplicates('EVENT_TEXT')
-# >>> len(deduped)
-# 320946
-# >>> deduped.to_csv('1606251790_0B3CC61FD4764094EAEA375353022009_G3486172_deduped.csv', index=False)
 import pandas as pd
 
-FILE_NAME = '1606251790_0B3CC61FD4764094EAEA375353022009_G3486172'
+if __name__ == '__main__':
+    FILE_NAME = '../data/2020-08-11to2021-02-11'
 
-toclean = pd.read_csv(f'{FILE_NAME}.csv')
-deduped = toclean.drop_duplicates('RP_STORY_ID')
-deduped = deduped.drop_duplicates('EVENT_TEXT')
-deduped.to_csv(f'{FILE_NAME}_deduped.csv', index=False)
+    toclean = pd.read_csv(f'{FILE_NAME}.csv')
+    deduped = toclean.drop_duplicates('RP_STORY_ID')
+    deduped = deduped.drop_duplicates('EVENT_TEXT')
+    deduped = deduped.drop_duplicates('HEADLINE')
+    deduped = deduped[~deduped['EVENT_SENTIMENT_SCORE'].isna()]
+    deduped.to_csv(f'{FILE_NAME}_deduped.csv', index=False)
