530bcae95bfd54ad2e44c210d37777fee65d61d1
Asier <asierguti96@gmail.com>
2021-02-28 13:57:10 +0100

    Executables for testing with other data

---
+++ src/bin/main_baseline_bert_test.py
@@ -0,0 +1,72 @@
+import os
+import git
+import time
+import torch
+import logging
+import argparse
+import numpy as np
+from controller.data import load_data_test
+from controller.model import prepare_device, prepare_preliminary, train_model, test_model
+from model.baseline_bert_classifier import BaselineBERTClassifier
+
+"""
+Train and test a given model.
+"""
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    # Global parameters
+    parser.add_argument('data_path', help="Path to data.", type=str)
+    parser.add_argument('experiment_path', help="Path to experiment.", type=str)
+    parser.add_argument('dataset', help='Dataset to be computed.', type=str, choices=["finance", "phrasebank"])
+    parser.add_argument('checkpoint_path', type=str)
+    parser.add_argument('--experiment-name', help="Experiment name.", type=str, default='baseline_bert')
+
+    # Model architecture parameters
+    parser.add_argument('--number-layers', help="Number of layers of the model.", type=int, default=1)
+    parser.add_argument('--layer-size', help="Layer start size of the model.", type=int, default=256)
+    parser.add_argument('--minimum-layer-size', help="Layer minimum size of the model.", type=int, default=8)
+    parser.add_argument('--dropout-rate', help="Dropout that is applied to the model.", type=int, default=0.2)
+
+    # Model training parameters
+    parser.add_argument('--seed', help="Seed to be used for randomization purposes.", type=int, default=42)
+    parser.add_argument('--batch-size', help="Batch size to feed the model.", type=int, default=32)
+    parser.add_argument('--optimizer', help="Optimizer for training the model.", type=str, choices=['adam'],
+                        default='adam')
+    parser.add_argument('--lr', help="Learning rate for training the model.", type=float, default=0.001)
+    parser.add_argument('--no-cuda', help="Avoid using cuda.", action='store_true')
+    args = parser.parse_args()
+
+    # Experiments directory
+    repo = git.Repo(search_parent_directories=True)
+    sha = repo.head.object.hexsha
+    ts = time.time()
+    experiment_directory = os.path.join(args.experiment_path, f'{args.experiment_name}_{ts}_{sha[:7]}')
+    os.makedirs(experiment_directory, exist_ok=True)
+
+    # Logger
+    logging.basicConfig(filename=os.path.join(experiment_directory, 'process.log'), level=logging.INFO)
+    logging.getLogger('').addHandler(logging.StreamHandler())
+
+    # Seeds
+    torch.manual_seed(args.seed)
+    if not args.no_cuda:
+        torch.backends.cudnn.deterministic = True
+    # torch.set_deterministic(True)
+    np.random.seed(args.seed)
+
+    # Load data
+    data_loader_train, data_loader_valid, data_loader_test = load_data_test(dataset=args.dataset, path=args.data_path,
+                                                                       batch_size=args.batch_size)
+
+    # Build classifier
+    device = prepare_device(args.no_cuda)
+    classifier = BaselineBERTClassifier(model='bert-base-uncased', device=device, number_layers=args.number_layers,
+                                        layer_size=args.layer_size, minimum_layer_size=args.minimum_layer_size,
+                                        dropout_rate=args.dropout_rate)
+    classifier.to(device)
+
+    # Train and test
+    criterion, optimizer = prepare_preliminary(args, classifier)
+    classifier.load_state_dict(torch.load(os.path.join(args.checkpoint_path, 'checkpoint_best.pt')))
+    test_model(model=classifier, dataset=args.dataset, device=device, data_loader_test=data_loader_test,
+               criterion=criterion)
--- src/controller/data.py
+++ src/controller/data.py
@@ -4,10 +4,17 @@ from data.finantial_phrasebank_dataset import load as load_phrasebank
 
 def load_data(dataset, path, batch_size, ratio):
     ratios = [ratio]
-    validation_test_ratios = 1-ratio
+    validation_test_ratios = 1 - ratio
     validation_test_ratios /= 2
     ratios.extend([validation_test_ratios, validation_test_ratios])
     if dataset == "finance":
         return load_finance(path, batch_size, ratios)
     else:
         return load_phrasebank(path, batch_size, ratios)
+
+
+def load_data_test(dataset, path, batch_size):
+    if dataset == "finance":
+        return load_finance(path, batch_size, [0.0, 0.0, 1.0])
+    else:
+        return load_phrasebank(path, batch_size, [0.0, 0.0, 1.0])
